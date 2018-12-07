9f804c266d1f53c4fdfc54e8e1c76f6493c17b70
Claudio Andaur <malefico3d@gmail.com>
2018-12-07 13:26:21 -0300

    Agregada escena 2. Separados carteles de error y muybien del div netbook

--- digiaventuras.js
+++ digiaventuras.js
@@ -1,7 +1,7 @@
 var escena_actual = 1;
 var elemento_actual = 1;
 
-class Escena {
+/*class Escena {
 
   constructor(inicial = 1, total = 1) {
     this.actual = inicial;
@@ -22,9 +22,9 @@ class Escena {
     }
     return this.actual;
   }
-}
+}*/
 
-function mostrarProximaEscena() {
+function resetEscenas(){
 	for (var e=0; e<3; e++) {
 		var id = "escena-" + e;
 		var escena = document.getElementById(id);
@@ -32,18 +32,19 @@ function mostrarProximaEscena() {
 			escena.style.display='none';
 		}
 	}
+}
 
-	var id = "escena-" + escena_actual;
+function mostrarEscena(e) {
+	var id = "escena-" + e;
 	var escena = document.getElementById(id);
 	if (escena != null) { 
 		escena.style.display='block';
-		escena_actual++;
-		if (escena_actual > 2){ escena_actual=0; }
 	}
 }
 
+
 function resetCarteles(){
-	for (var c=1; c<6; c++) {
+	for (var c=1; c<8; c++) {
 		var id = "cartel" + c;
 		var cartel = document.getElementById(id);
 		if (cartel != null) { 
@@ -63,7 +64,10 @@ function mostrarCartel(){
 function mostrarProximoElemento() {
 	resetCarteles();
 	elemento_actual++;
-	if (elemento_actual > 5){ elemento_actual=1; }
+	if (elemento_actual == 6 || elemento_actual==8){ 
+		escena_actual+=1;
+		mostrarEscena(escena_actual);
+		}
 	mostrarCartel();
 }
 
@@ -72,26 +76,21 @@ function chequearElemento(id) {
 	var n = id.length;
 	var e = id.charAt(n-1);
 	if (e != elemento_actual){
-		/*alert("No es el elemento");*/
 		mostrarVentana("error");
 	}
 	else {
 		mostrarVentana("muybien");
-		/*elemento_actual+=1;
-		if(elemento_actual>5){elemento_actual=1;}*/
 		mostrarProximoElemento();
 	}
 }
 
 function mostrarVentana(id) {
-	/*alert(id);*/
 	var ventana = document.getElementById(id);
 	ventana.style.animation = 'aparece 300ms linear';
 	ventana.style.display = 'block';
 }
 
 function cerrarVentana(id) {
-	/*alert(id);*/
 	var ventana = document.getElementById(id);
 	ventana.style.animation = 'desaparece 100ms linear';
 	ventana.style.display = 'none';
